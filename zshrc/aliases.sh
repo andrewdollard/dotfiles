fd0ae5bdd04528ad3b5201e8c4f1aa842dcf6d87
Andrew Dollard <andrewdollard@gmail.com>
2024-05-22 08:00:42 -0700

    alias gcw

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -2,6 +2,7 @@ alias gh='git hist -n20'
 alias gs='git status'
 alias gd='git diff'
 alias gds='git diff --staged'
+alias gcw="git add . && git commit -m 'wip'"
 
 alias tree='lsd --tree'
 alias ls='lsd'
