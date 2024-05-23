a499fc4732411b65f5d7f31b9e8fcacdeaf7a284
Andrew Dollard <adollard@patreon.com>
2024-05-23 15:24:58 -0700

    gmm alias

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -2,6 +2,8 @@ alias gh='git hist -n20'
 alias gs='git status'
 alias gd='git diff'
 alias gds='git diff --staged'
+alias gmm='git checkout master && git pull && git checkout - && git merge master'
+
 alias sed='gsed'
 alias ls='lsd'
 alias ll='lsd -lah'
