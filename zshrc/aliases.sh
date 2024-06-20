26a857a1586bb6c4841bc21e5ef42bdaa30ce6ee
Andrew Dollard <adollard@patreon.com>
2024-06-20 08:06:26 -0700

    tweak gmm alias

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -3,7 +3,7 @@ alias gs='git status'
 alias gd='git diff'
 alias gds='git diff --staged'
 alias gcw="git add . && git commit -m 'wip'"
-alias gmm='git checkout master && git pull && git checkout - && git merge master'
+alias gmm='git checkout master && git pull && git checkout - && GIT_MERGE_AUTOEDIT=no git merge master'
 alias gb="git for-each-ref --sort='-committerdate' --format='%(committerdate:short)%20%20%(refname:lstrip=2)' refs/heads | head -n 20"
 
 alias sed='gsed'
