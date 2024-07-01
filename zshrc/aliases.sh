fb6935a6eb925baaf9039841166f4dbf05215f10
Andrew Dollard <adollard@patreon.com>
2024-06-30 18:24:02 -0700

    update gmm

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -5,7 +5,7 @@ alias gs='git status'
 alias gp='git push'
 alias gds='git diff --staged'
 alias gcw="git add . && git commit -m 'wip'"
-alias gmm='git fetch origin master && GIT_MERGE_AUTOEDIT=no git merge origin/master'
+alias gmm='git fetch origin master:master && GIT_MERGE_AUTOEDIT=no git merge master'
 alias gb="git for-each-ref --sort='-committerdate' --format='%(committerdate:short)%20%20%(refname:lstrip=2)' refs/heads | head -n 20"
 
 alias sed='gsed'
