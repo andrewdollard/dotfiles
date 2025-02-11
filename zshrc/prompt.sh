f5c697c43b0c9ed46f4c34f8e2c3cbc9d8405d5b
Andrew Dollard <andrewdollard@gmail.com>
2025-02-11 10:04:08 -0700

    add pyenv venv to prompt

--- zshrc/prompt.sh
+++ zshrc/prompt.sh
@@ -1,3 +1,5 @@
+setopt PROMPT_SUBST
+
 function git_branch_name() {
   branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
   if [[ $branch == "" ]];
@@ -8,6 +10,8 @@ function git_branch_name() {
   fi
 }
 
-setopt PROMPT_SUBST
 export PROMPT='%? %F{2}%m%f%F{green}:%f%F{10}%~%f%B%F{12}$(git_branch_name)%f%b $ '
 
+if [[ "$(pyenv virtualenvs)" == *"* $(pyenv version-name) "* ]]; then
+	export PROMPT='($(pyenv version-name)) '$PROMPT
+fi
