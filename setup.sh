59fdd1e79553ff469c223ed16f9920bee7bc71b5
Andrew Dollard <andrewdollard@gmail.com>
2025-07-10 12:46:12 -0700

    preserve existing dotfiles on install

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ dotfiles_dir=$(cd "$(dirname "$0")"; pwd)
 
 for file in $(ls -d home/.??*); do
   base=$(basename "$file")
-  rm -rf "${HOME}/${base}"
+  mv "${HOME}/${base}" "${HOME}/${base}.old"
   ln -s "${dotfiles_dir}/${file}" "${HOME}/${base}"
 done
 
 for file in $(ls -d scripts/*); do
   base=$(basename "$file")
-  rm -rf "/usr/local/bin/${base}"
+  mv "/usr/local/bin/${base}" "/usr/local/bin/${base}.old"
   ln -s "${dotfiles_dir}/${file}" "/usr/local/bin/${base}"
 done
