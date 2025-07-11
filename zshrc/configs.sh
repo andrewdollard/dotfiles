673da05822e65fd1ed8f22d6d570c6fa8b1805fc
Andrew Dollard <andrewdollard@gmail.com>
2025-07-10 17:09:01 -0700

    move mac env var to mac file

--- zshrc/configs.sh
+++ zshrc/configs.sh
@@ -1,4 +1,2 @@
-export HOMEBREW_NO_AUTO_UPDATE=1
-
 export LESS='--ignore-case --QUIET --raw-control-chars --no-init --quit-if-one-screen'
 export LESSSECURE=1
--- zshrc/mac.sh
+++ zshrc/mac.sh
@@ -1,2 +1,4 @@
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
